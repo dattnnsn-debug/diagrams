7b68a22d274ecd6c2f2a2cff5a73bd0cacc5c1d4
Tetiana <dattnnsn@gmail.com>
2026-01-06 15:26:51 +0200

    Автоматичне збереження діаграм

--- scatter_squares.py
+++ scatter_squares.py
@@ -17,4 +17,7 @@ ax.tick_params(axis='both', which='major', labelsize=14)
 
 '''Задати діапазон для кожної з осей.'''
 ax.axis([0, 1100, 0, 1100000])
+
+'''Автоматичне збререження діаграми'''
+plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()
