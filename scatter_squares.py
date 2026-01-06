5fde136bec1507029dc28227ae00c77b702c0cf1
Tetiana <dattnnsn@gmail.com>
2026-01-06 15:22:05 +0200

    Графік на 1000 значень, зміна кольру

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=200)
+ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=10)
 
 '''Задати назву для графіка та кожної з осей.'''
 ax.set_title("Square Numbers", fontsize = 24)
@@ -14,4 +14,7 @@ ax.set_ylabel("Square of Value", fontsize = 14)
 
 '''Задати розмір шрифта для підписів на розмітці.'''
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+'''Задати діапазон для кожної з осей.'''
+ax.axis([0, 1100, 0, 1100000])
 plt.show()
