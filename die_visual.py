557ecc6a3d71acf671af04f343f657774628e659
Tetiana <dattnnsn@gmail.com>
2026-01-14 14:36:37 +0200

    Кидаємо 1000 разів, будуємо діаграму через plotly

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,6 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 '''Створити D6'''
@@ -16,3 +19,13 @@ for value in range(1, die.num_sides+1):
     frequencies.append(frequency)
 
 print(frequencies)
+
+'''Візуалізувати результати.'''
+x_values = list(range(1, die.num_sides + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title='Result of rolling one D6 1000 times',
+                   xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
