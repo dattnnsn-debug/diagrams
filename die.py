65f4204927c18c435eb7de30115ff4c592457b83
Tetiana <dattnnsn@gmail.com>
2026-01-14 14:20:11 +0200

    Створюєм клас Die, створюємо візуал для кидання кубика 100 разів

---
+++ die.py
@@ -0,0 +1,11 @@
+from random import randint
+
+class Die:
+    '''Клас репрезентує один кубик.'''
+
+    def __init__(self, num_sides=6):
+        '''Визначити кубик з шістьома гранями.'''
+        self.num_sides = num_sides
+    def roll(self):
+        '''Повернути випадкове значення від 1 до 6.'''
+        return randint(1, self.num_sides)
